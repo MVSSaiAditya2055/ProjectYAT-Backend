ac29595239c7e5130167943673422fe87f430d0c
MVSSaiAditya2055 <2400032055@kluniversity.in>
2026-04-07 17:21:16 +0530

    done

--- src/main/java/com/klu/ProjectYAT/model/User.java
+++ src/main/java/com/klu/ProjectYAT/model/User.java
@@ -1,8 +1,10 @@
 package com.klu.ProjectYAT.model;
 
-import jakarta.persistence.*;
-import java.util.HashSet;
-import java.util.Set;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 @Entity
 @Table(name="users")
 public class User {
